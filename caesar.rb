dc51a96723a580631330225eef101edbf807e254
Jonathan <parrottjonathanc@gmail.com>
2020-01-02 11:26:56 -0700

    I1: Created simple Sinatra frontend for Caesar_cipher.rb

--- caesar.rb
+++ caesar.rb
@@ -3,21 +3,28 @@ require 'sinatra/reloader'
 
 # 65-90 upper
 # 97-122
-
-def caesar_cyper(string, num)
-  new_string = string.split("").map do |char|
-    if char.ord >= 65 && char.ord <= 90
-      char = (((char.ord + num - 65) % 26) + 65).ord
-    elsif char.ord >= 97 && char.ord <= 122
-      char = (((char.ord + num - 97) % 26) + 122).ord
+def caesar_cipher(phrase, offset)
+  cipher = ""
+  lc_alpha = "abcdefghijklmnopqrstuvwxyz"
+  uc_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  phrase.split("").map do |char|
+    if lc_alpha.include?(char)
+      cipher.concat << lc_alpha.chars[(lc_alpha.index(char).to_i + offset) % 26]
+    elsif uc_alpha.include?(char)
+      cipher.concat << uc_alpha.chars[(uc_alpha.index(char).to_i + offset) % 26]
     else
-      char = char
+      cipher.concat << char
     end
   end
-
-  puts new_string.to_s
-
+  cipher
 end
 
+
+get '/' do
+  $secret = params['secret']
+  $shift = params['shift'].to_i
+  $encoded = caesar_cipher($secret, $shift)
+  erb :index,{ :locals => params, :secret => $secret, :shift => $shift, :encoded => $encoded }
+end
 
   
